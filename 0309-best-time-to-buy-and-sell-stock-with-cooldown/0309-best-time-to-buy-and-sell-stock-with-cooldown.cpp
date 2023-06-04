05087a5cde839c333e576761e05548186360b072
Piyush Singh <108541125+piyush12nov@users.noreply.github.com>
2023-06-04 18:35:08 +0530

    Time: 0 ms (100.00%), Space: 11.9 MB (37.51%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,24 +1,43 @@
 class Solution {
 public:
-    int dp[5005][1005][3];
-    int ff(int i,int prev,vector<int>&v,int type)
+    // int dp[5005][1005][3];   time comp= n*max_element*type; correct
+    // int ff(int i,int prev,vector<int>&v,int type)
+    // {
+    //     if(i>=v.size())return 0;
+    //     if(dp[i][prev][type]!=-1)return dp[i][prev][type];
+    //     int a=0,b=0,c=0;
+    //     if(type==0)
+    //     {
+    //         a=ff(i+1,v[i],v,1);
+    //     }
+    //     if(type==1&&v[i]>=prev)
+    //     {
+    //         b=v[i]-prev+ff(i+2,0,v,0);
+    //     }
+    //     c=ff(i+1,prev,v,type);
+    //     return dp[i][prev][type]=max({a,b,c});
+    // }
+    int dp[5005][3];
+    int n;
+    int ff(int i,int type,vector<int>&a)
     {
-        if(i>=v.size())return 0;
-        if(dp[i][prev][type]!=-1)return dp[i][prev][type];
-        int a=0,b=0,c=0;
+        if(i>=n)return 0;
+        if(dp[i][type]!=-1)return dp[i][type];
+        int ab=0,c=0;
         if(type==0)
         {
-            a=ff(i+1,v[i],v,1);
+            ab=-1*a[i]+ff(i+1,1,a);
         }
-        if(type==1&&v[i]>=prev)
+        if(type==1)
         {
-            b=v[i]-prev+ff(i+2,0,v,0);
+            ab=a[i]+ff(i+2,0,a);
         }
-        c=ff(i+1,prev,v,type);
-        return dp[i][prev][type]=max({a,b,c});
+        c=ff(i+1,type,a);
+        return dp[i][type]=max({ab,c});
     }
     int maxProfit(vector<int>& prices) {
         memset(dp,-1,sizeof(dp));
-        return ff(0,0,prices,0);
+        n=prices.size();
+        return ff(0,0,prices);
     }
 };
