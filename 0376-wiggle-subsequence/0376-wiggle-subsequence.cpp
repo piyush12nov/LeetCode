70586d46fdf696e6ede200955a71718735011d44
Piyush Singh <108541125+piyush12nov@users.noreply.github.com>
2023-06-05 18:05:27 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (29.86%) - LeetHub

--- 0376-wiggle-subsequence/0376-wiggle-subsequence.cpp
+++ 0376-wiggle-subsequence/0376-wiggle-subsequence.cpp
@@ -1,36 +1,42 @@
-class Solution {
-public:
-    int dp[1005][3];
-    int ff(int i,int type,vector<int>&a,int prev)
-    {
-        int n=a.size();
-        
-        if(i>=n)return 0;
-        if(dp[i][type]!=-1)return dp[i][type];
-        
-        int aa=0,b=0,x=0,y=0,z=0;
-        
-        if(type==0&&prev<a[i])
-        {
-            x=ff(i+1,1,a,a[i]);
-            aa=1+x;
-        }
-        if(type==1&&prev>a[i])
-        {
-            y=ff(i+1,0,a,a[i]);
-            b=1+y;
-        }
-        if(type==0)y=ff(i+1,0,a,a[i]);
-        if(type==1)x=ff(i+1,1,a,a[i]);
-  //      z=ff(i+1,type,a,prev);
-        dp[i][type]=max({aa,x,b,y,z});      
-        return dp[i][type];
-    }
-    int wiggleMaxLength(vector<int>& nums) {
-        memset(dp,-1,sizeof(dp));
-        int aa1=ff(0,0,nums,0);
-        memset(dp,-1,sizeof(dp));
-        int aa2=ff(0,1,nums,1001);
-        return max(aa1,aa2);
-    }
+class Solution
+{
+	public:
+	int dp[1005][3];
+    int n;
+	
+    int ff(int i, int type, vector<int> &a, int prev)
+	{
+		if (i >= n) return 0;
+		
+        if (dp[i][type] != -1) return dp[i][type];
+
+		int aa = 0, b = 0, x = 0, y = 0;
+
+		if (type == 0 && prev < a[i])
+		{
+			x = ff(i + 1, 1, a, a[i]);
+			aa = 1 + x;
+		}
+
+		if (type == 1 && prev > a[i])
+		{
+			y = ff(i + 1, 0, a, a[i]);
+			b = 1 + y;
+		}
+
+		if (type == 0) y = ff(i + 1, 0, a, a[i]);
+		if (type == 1) x = ff(i + 1, 1, a, a[i]);
+		
+        return dp[i][type] = max({ aa, x, b, y });
+	}
+
+	int wiggleMaxLength(vector<int> &nums)
+	{
+		n = nums.size();
+		memset(dp, -1, sizeof(dp));
+		int aa1 = ff(0, 0, nums, 0);
+		memset(dp, -1, sizeof(dp));
+		int aa2 = ff(0, 1, nums, 1001);
+		return max(aa1, aa2);
+	}
 };
