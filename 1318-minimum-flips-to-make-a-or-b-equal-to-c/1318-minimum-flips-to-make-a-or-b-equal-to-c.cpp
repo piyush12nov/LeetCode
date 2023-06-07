2b38882d34288db60fe152c1a0fcae34d7284d20
Piyush Singh <108541125+piyush12nov@users.noreply.github.com>
2023-06-07 11:37:32 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (53.28%) - LeetHub

--- 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.cpp
+++ 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.cpp
@@ -3,25 +3,19 @@ class Solution
     public:
         int minFlips(int x, int y, int z)
         {
-            int i, ans = 0;
-            
-            vector<int> a(32, 0), b(32, 0), c(32, 0);
+            int i,a,b,c, ans = 0;  
             
             for (i = 0; i < 32; i++)
             {
-                a[i] = x % 2;
+                a = x % 2;
                 x = x / 2;
-                b[i] = y % 2;
+                b = y % 2;
                 y = y / 2;
-                c[i] = z % 2;
+                c = z % 2;
                 z = z / 2;
-            }
-            
-            for (i = 0; i < 32; i++)
-            {
-                if (a[i] + b[i] == 1 && c[i] == 0) ans++;
-                if (a[i] + b[i] == 2 && c[i] == 0) ans += 2;
-                if (a[i] + b[i] == 0 && c[i] == 1) ans++;
+                if (a + b == 1 && c == 0) ans++;
+                if (a + b == 2 && c == 0) ans += 2;
+                if (a + b == 0 && c == 1) ans++;
             }
             
             return ans;
