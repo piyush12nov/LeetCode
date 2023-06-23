3c0f968019c1c8be0bd325f048db5b5a4f9e6adb
Piyush Singh <108541125+piyush12nov@users.noreply.github.com>
2023-06-23 14:29:50 +0530

    Time: 183 ms (98.35%), Space: 84.1 MB (66.24%) - LeetHub

--- 1944-number-of-visible-people-in-a-queue/1944-number-of-visible-people-in-a-queue.cpp
+++ 1944-number-of-visible-people-in-a-queue/1944-number-of-visible-people-in-a-queue.cpp
@@ -2,26 +2,25 @@ class Solution {
 public:
     vector<int> canSeePersonsCount(vector<int>& v) {
         int n=v.size();
-        vector<int>ans(n,0);
+        vector<int>ans(n,1);
+        ans[n-1]=0;
         stack<int>s;
-      //  s.push(v[n-1]);
-        for(int i=n-1;i>=0;i--)
+        s.push(v[n-1]);
+        for(int i=n-2;i>=0;i--)
         {
             int x=v[i];
-            cout<<i<<" = I "<<" ";
+            int y=1;
             while(s.size()>=1&&x>s.top())
             {
-                ans[i]++;
+                y++;
                 s.pop();
-            }   
-            if(!s.empty())
-            ans[i]++;
-         
-                s.push(x);
+            }
             
+            if(s.empty())
+            ans[i]=y-1;
+            else ans[i]=y;
+            s.push(x);
         }
-        ans[n-1]=0;
-
         return ans;
     }
 };
