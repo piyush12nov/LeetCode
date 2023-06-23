d76212d8cf3ef1b5c18ae784487af4fce530c487
Piyush Singh <108541125+piyush12nov@users.noreply.github.com>
2023-06-23 13:09:48 +0530

    Time: 393 ms (68.21%), Space: 207 MB (61.86%) - LeetHub

--- 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.cpp
+++ 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.cpp
@@ -1,23 +1,28 @@
-class Solution {
-public:
+class Solution
+{
+    public:
 
-    int longestArithSeqLength(vector<int>& nums) {
-        int i,n=nums.size(),j;
-        int ma=*max_element(nums.begin(),nums.end());
-        vector<vector<int>>dp(nums.size()+1,vector<int>(505+ma,1));
-        for(i=0;i<n;i++)
+        int longestArithSeqLength(vector<int> &nums)
         {
-            for(j=0;j<i;j++)
+            int i, n = nums.size(), j;
+            int ma = *max_element(nums.begin(), nums.end());
+            vector<vector < int>> dp(nums.size() + 1, vector<int> (505 + ma, 1));
+            
+            for (i = 0; i < n; i++)
             {
-                int ss=500+nums[i]-nums[j];
-                dp[i][ss]=max(dp[i][ss],dp[j][ss]+1);
+                for (j = 0; j < i; j++)
+                {
+                    int ss = 500 + nums[i] - nums[j];
+                    dp[i][ss] = max(dp[i][ss], dp[j][ss] + 1);
+                }
             }
+            int ans = 0;
+            
+            for (auto x: dp)
+            {
+                for (auto it: x) ans = max(ans, it);
+            }
+            
+            return ans;
         }
-        int ans=0;
-        for(auto x:dp)
-        {
-            for(auto it:x)ans=max(ans,it);
-        }
-        return ans;
-    }
 };
