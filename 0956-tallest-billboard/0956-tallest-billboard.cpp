07d6c4b0b3ea6fea96d4eb88ea624aa2b2821560
Piyush Singh <108541125+piyush12nov@users.noreply.github.com>
2023-06-24 16:37:13 +0530

    Time: 58 ms (85.02%), Space: 8.3 MB (93.83%) - LeetHub

--- 0956-tallest-billboard/0956-tallest-billboard.cpp
+++ 0956-tallest-billboard/0956-tallest-billboard.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
     int dp[23][5005];
-    int ff(int i,int a,int b,vector<int>&v,vector<int>&pre)
+    int ff(int i,int a,int b,vector<int>&v)
     {
         if(i==v.size())
         {
             if(a==b)return 0;
             else return -1*99999;
         }
-        
         if(dp[i][abs(a-b)]!=-1)return dp[i][abs(a-b)];
-        int x=v[i]+ff(i+1,a+v[i],b,v,pre);
-        int y=v[i]+ff(i+1,a,b+v[i],v,pre);
-        int z=ff(i+1,a,b,v,pre);
+        int x=v[i]+ff(i+1,a+v[i],b,v);
+        int y=v[i]+ff(i+1,a,b+v[i],v);
+        int z=ff(i+1,a,b,v);
         return dp[i][abs(a-b)]=max({x,y,z});
     }
     int tallestBillboard(vector<int>& rods) {
         memset(dp,-1,sizeof(dp));
-        vector<int>pre;
-        return ff(0,0,0,rods,pre)/2;
+        return ff(0,0,0,rods)/2;
     }
 };
