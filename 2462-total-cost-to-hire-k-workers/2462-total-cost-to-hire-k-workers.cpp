6d68073a19012591aa109e5da07032d68f4141cd
Piyush Singh <108541125+piyush12nov@users.noreply.github.com>
2023-06-26 15:23:44 +0530

    Time: 203 ms (69.75%), Space: 72.8 MB (61.12%) - LeetHub

--- 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.cpp
+++ 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.cpp
@@ -1,4 +1,3 @@
-
 #define ll long long int
 class Solution
 {
@@ -7,68 +6,49 @@ class Solution
         {
             ll n = a.size();
             ll l = 0, r = n - 1, ans = 0, flag = 0;
-            vector<int> vis(n, 0);
             priority_queue<int, vector < int>, greater < int>> p1, p2;
-            while (l < k && !vis[l])
+            while (l < k && l <= r)
             {
                 p1.push(a[l]);
-                vis[l] = 1;
-                l++;        
-                flag=1;
+                l++;
             }
-            if(flag)
-            {
-                flag=0;
-                l--;
-            }
-            while (r >= n - k && !vis[r])
+
+            while (r >= n - k && l <= r)
             {
                 p2.push(a[r]);
-                vis[r] = 1;
                 r--;
-                flag=1;
             }
-            if(flag){
-                
-            r++;}
+
             while (can)
             {
-                ll x=INT_MAX,y=INT_MAX;
-              //  cout<<p1.size()<<" size "<<p2.size()<<endl;
-                if(p1.size()>=1)
-                 x = p1.top();
-                if(p2.size()>=1)
-                 y = p2.top();
-              //  cout<<x<<"  "<<y<<endl;
+                ll x = INT_MAX, y = INT_MAX;
+                if (p1.size() >= 1)
+                    x = p1.top();
+                if (p2.size() >= 1)
+                    y = p2.top();
                 if (x <= y)
                 {
-             //   cout<<x<<"  reladle  "<<y<<endl;
-
                     can--;
                     ans += x;
                     p1.pop();
-                    l++;
-                    if (l<n&&!vis[l] )
+                    if (l <= r)
                     {
                         p1.push(a[l]);
-                        vis[l] = 1;
+                        l++;
                     }
                 }
                 else
                 {
-               //     cout<<x<<"  43443reladle  "<<y<<endl;
                     can--;
                     ans += y;
                     p2.pop();
-                    r--;
-                    if (r >= 0 && !vis[r])
+                    if (l <= r)
                     {
                         p2.push(a[r]);
-                        vis[r] = 1;
+                        r--;
                     }
                 }
             }
-         //   cout<<endl;
             return ans;
         }
 };
