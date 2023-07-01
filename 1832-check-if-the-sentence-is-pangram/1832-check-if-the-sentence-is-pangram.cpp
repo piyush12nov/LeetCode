dab8a9e559a346579fbe16e197a1e0a62244cbb2
Piyush Singh <108541125+piyush12nov@users.noreply.github.com>
2023-07-01 21:12:39 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (96.80%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -3,8 +3,6 @@ public:
     bool checkIfPangram(string s) {
         int cnt[26];
         memset(cnt,0,sizeof(cnt));
-  //      sort(s.begin(),s.end());
-        cout<<s<<" "<<endl;
         for(int i=0;i<s.size();i++)
         {
             cnt[s[i]-'a']++;
