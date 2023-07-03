a8968c630374890b1f717a0c8bd3a637dc40e9d7
Piyush Singh <108541125+piyush12nov@users.noreply.github.com>
2023-07-03 16:18:02 +0530

    Time: 10 ms (13.06%), Space: 7 MB (52.51%) - LeetHub

--- 0859-buddy-strings/0859-buddy-strings.cpp
+++ 0859-buddy-strings/0859-buddy-strings.cpp
@@ -3,46 +3,44 @@ class Solution
     public:
         bool buddyStrings(string a, string b)
         {
-            if(a.size()!=b.size())return 0;
+            if (a.size() != b.size()) return 0;
             string s;
-            map<string,int>m,mm;
-            bool flag=false,fl=false;
-            int cnt=0;
+            unordered_map<string, int> m, mm;
+            bool flag = false, fl = false;
+            int cnt = 0;
             for (int i = 0; i < a.size(); i++)
             {
-                
-                int x=a[i]-'a',y=b[i]-'a';
-                cout<<x<<" "<<y<<endl;
+
+                int x = a[i] - 'a', y = b[i] - 'a';
                 if (a[i] != b[i])
                 {
-                       s='a'+x;
-                s+='a'+y;
-                m[s]++;
+                    s = 'a' + x;
+                    s += 'a' + y;
+                    m[s]++;
                     cnt++;
                 }
-                else{
-                       s='a'+x;
-                s+='a'+y;
-                mm[s]++;
+                else
+                {
+                    s = 'a' + x;
+                    s += 'a' + y;
+                    mm[s]++;
                 }
-             
-              //..  cout<<s<<" ";
+
+               	//..  cout<<s<<" ";
             }
-            if(cnt%2==1)return 0;
-            for(auto [x,y]:m)
+            if (cnt % 2 == 1) return 0;
+            for (auto[x, y]: m)
             {
-                string s=x;
-              //  cout<<x<<" ";
-                reverse(s.begin(),s.end());
-                cout<<s<<" "<<x<<endl;
-                if(m.find(s)!=m.end())fl=true;
+                string s = x;
+                reverse(s.begin(), s.end());
+                if (m.find(s) != m.end()) fl = true;
             }
-            for(auto [x,y]:mm)
+            for (auto[x, y]: mm)
             {
-                if(x[0]==x[1]&&y>=2)flag=true;
-}
-            if(cnt==0&&flag)return 1;
-            if(cnt==2&&fl)return 1;
+                if (x[0] == x[1] && y >= 2) flag = true;
+            }
+            if (cnt == 0 && flag) return 1;
+            if (cnt == 2 && fl) return 1;
             return 0;
         }
 };
