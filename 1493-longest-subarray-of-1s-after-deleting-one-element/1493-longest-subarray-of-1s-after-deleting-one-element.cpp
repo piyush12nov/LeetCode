9525d4967469ab0d7581b4215abdfa39080154f6
Piyush Singh <108541125+piyush12nov@users.noreply.github.com>
2023-07-05 23:44:32 +0530

    Time: 49 ms (40.22%), Space: 36.5 MB (65.98%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -4,7 +4,6 @@ class Solution
         int longestSubarray(vector<int> &a)
         {
             int n = a.size(), i = 0, prev = 0, ans = 0, zero = 0, one = 0;
-            vector<int> v;
             while (i < n)
             {
                 zero = 0;
@@ -16,16 +15,14 @@ class Solution
                         i++;
                         zero++;
                     }
-                    v.push_back(zero);
                 }
-                if (i<n&&a[i] == 1)
+                if (i < n && a[i] == 1)
                 {
                     while (i < n && a[i] == 1)
                     {
                         i++;
                         one++;
                     }
-                    v.push_back(one);
                     if (one != n) ans = max(ans, one);
                     if (zero == 1)
                     {
@@ -35,8 +32,6 @@ class Solution
                     prev = one;
                 }
             }
-            for (auto x: v) cout << x << " ";
-            cout << endl;
             return ans;
         }
 };
