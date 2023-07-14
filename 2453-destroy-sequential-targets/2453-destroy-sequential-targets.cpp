b4a1b859567fe9068e50bd4e8b28f314b7008fc6
Piyush Singh <108541125+piyush12nov@users.noreply.github.com>
2023-07-14 23:00:43 +0530

    Time: 339 ms (20.14%), Space: 77.3 MB (49.23%) - LeetHub

--- 2453-destroy-sequential-targets/2453-destroy-sequential-targets.cpp
+++ 2453-destroy-sequential-targets/2453-destroy-sequential-targets.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
     int destroyTargets(vector<int>& nums, int space) {
-        int n=nums.size(),an=0,ans=INT_MAX;
-        unordered_map<int, int>m;
+        map<int,int>m;
+        int ans=0,res=nums[0],result=INT_MAX;
+       
         for(auto x:nums)
         {
             m[x%space]++;
-             an=max(an,m[x%space]);
-        }
-        for(auto x:nums)
-        {
-            if(an==m[x%space])
+            if(ans<m[x%space])
             {
-                ans=min(x,ans);
+                ans=max(ans,m[x%space]);   
             }
         }
-        return ans;
+        for(int i = 0; i < nums.size() ; i++)
+        {
+            if(m[nums[i]%space]==ans)
+            {
+                result=min(result,nums[i]);
+            }
+        }
+        return result;
     }
 };
