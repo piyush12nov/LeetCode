5dcaffa95a543163dd80c01dda36646fa1e52068
Piyush Singh <108541125+piyush12nov@users.noreply.github.com>
2023-08-11 10:33:19 +0530

    Time: 12 ms (89.42%), Space: 11.4 MB (42.19%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -1,52 +1,62 @@
 /**
  *Definition for singly-linked list.
  *struct ListNode {
- *   int val;
- *   ListNode * next;
- *   ListNode() : val(0), next(nullptr) {}
- *   ListNode(int x) : val(x), next(nullptr) {}
- *   ListNode(int x, ListNode *next) : val(x), next(next) {}
+ *    int val;
+ *    ListNode * next;
+ *    ListNode() : val(0), next(nullptr) {}
+
+ *    ListNode(int x) : val(x), next(nullptr) {}
+
+ *    ListNode(int x, ListNode *next) : val(x), next(next) {}
+
  *};
+
  */
 class Solution
 {
-    public:
-        ListNode* rev(ListNode* &head, int k)
-        {
-            ListNode *prev = nullptr;
-            while (head && k--)
-            {
-                ListNode *rr = head->next;
-                head->next = prev;
-                prev = head;
-                head = rr;
-            }
-            return prev;
-        }
-    ListNode* reverseKGroup(ListNode *head, int k)
-    {
+	public:
+		ListNode* rev(ListNode* &head, int k)
+		{
+			ListNode *prev = nullptr;
+			while (head && k--)
+			{
+				ListNode *temp = head->next;
+				head->next = prev;
+				prev = head;
+				head = temp;
+			}
 
-        ListNode *f = head;
-        ListNode *aa = new ListNode(0);
-        ListNode *you = aa;
-        int n = 0;
-        while (f)
-        {
-            n++;
-            f = f->next;
-        }
-        int kk, nn = n;
-        while (n >= k)
-        {
-            ListNode *temp = rev(head, k);
-            aa->next = temp;
-            kk = k;
-            while (kk--) aa = aa->next;
-            n = n - k;
-        }
-        if (nn % k != 0)
-            aa->next = head;
+			return prev;
+		}
 
-        return you->next;
-    }
+	ListNode* reverseKGroup(ListNode *head, int k)
+	{
+		ListNode *ans = new ListNode(0);
+		ListNode *at = ans;
+		while (head)
+		{
+			ListNode *yt = head;
+			int h = k;
+			while (yt && h--)
+			{
+				yt = yt->next;
+			}
+
+			if (h <= 0)
+			{
+				ans->next = rev(head,k);
+				while (ans->next)
+				{
+					ans = ans->next;
+				}
+			}
+			else
+			{
+				ans->next = head;
+				break;
+			}
+		}
+
+		return at->next;
+	}
 };
